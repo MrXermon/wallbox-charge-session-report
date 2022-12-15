850389499395cfeb2e8830ebb2d29fa4a4c05908
Jan Gilla <jan@jangilla.de>
2022-12-15 14:03:42 +0100

    Rewrote car data model. Added output options for CLI and PDF.

--- config.yml.tpl
+++ config.yml.tpl
@@ -1,10 +1,18 @@
 ---
-app:
+power:
+  supplier: Rheinhessische Energie- und Wasserversorgungs-GmbH
+  tarif: Ökostrom
+  model: Mennekes Amtron
   cost: 0.41
 wallbox:
   ip: 192.168.0.99
   user: operator
   password: operator
-rfid_tags:
+cars:
   - sn: 04116b1aea6f85
     plate: "MZYV66E"
+    type: Hyundai Kona E
+    debitor: |
+      level66.network UG (haftungsbeschränkt)
+      Im Kirschgarten 9
+      55263 Ingelheim am Rhein
--- wallbox-charge-session-report.py
+++ wallbox-charge-session-report.py
@@ -13,6 +13,10 @@ import hashlib
 import csv
 import datetime
 
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import Table
+
 # Function to load and validate yml configuration.
 def config_load(path):
     # Check if file exists.
@@ -31,12 +35,12 @@ def config_load(path):
         return(False)
 
 # Lookup plate from list of rfid tags.
-def lookup_plate(tags, sn):
-    plate = ''
-    for tag in tags:
-        if tag['sn'] == sn:
-            plate = tag['plate']
-    return(plate)
+def lookup_car(cars, sn):
+    r_car = None
+    for car in cars:
+        if car['sn'] == sn:
+            r_car = car
+    return(r_car)
 
 
 # Main function including the magic.
@@ -52,11 +56,18 @@ if __name__ == '__main__':
         type=argparse.FileType('r', encoding='UTF-8')
     )
 
+    # Add argument for output configuration.
+    args_parser.add_argument(
+        '--output',
+        help='Options: cli, pdf',
+        default='cli'
+    )
+
     # Add argument to define start date.
     args_parser.add_argument(
         '--startdate',
         help='Start date to gather data (format: YYYY-MM-DD).',
-        default=str(datetime.date.today())
+        default=str(datetime.date.today().replace(day=1))
     )
 
     # Add argument to define end date.
@@ -98,7 +109,7 @@ if __name__ == '__main__':
             if r_login.status_code == 200 and r_login.json()['logged_in']:
                 print('Connection to Wallbox successful! Downloading report...')
                 session_id = r_login.json()['session']['id']
-                
+               
                 # Try to download CSV report from wallbox.
                 r_csv = requests.get(
                     'http://' + config['wallbox']['ip'] + '/export/csv',
@@ -106,18 +117,30 @@ if __name__ == '__main__':
                         'Authorization': session_id
                     }
                 )
-                
+
+                # Fetch additional information from wallbox.
+                r_manuf_serial_n = requests.get(
+                    'http://' + config['wallbox']['ip'] + '/rest/manuf_serial_n',
+                    headers={
+                        'Authorization': session_id
+                    }
+                )
+                if r_manuf_serial_n.status_code == 200 and r_manuf_serial_n.content:
+                    manuf_serial_n = r_manuf_serial_n.content
+                else:
+                    manuf_serial_n = ''
+
                 # Check if download succeded.
                 if r_csv.status_code == 200 and r_csv.content:
                     charge_sessions = {}
 
                     # Debug output.
-                    print('Download succeeded! Start parsing the data and filter per tag.')
+                    print('Download succeeded! Start parsing the data and filter per car.')
                     print()
 
-                    # Iterate through RFID tags and generate sum per tag.
-                    for tag in config['rfid_tags']:
-                        print('Search for sessions for tag: ' + tag['sn'])
+                    # Iterate through cars and generate sum per car.
+                    for car in config['cars']:
+                        print('Search for sessions for car: ' + car['sn'])
                         l_charge_sessions = []
 
                         # Load and parse csv once per tag to include all rows.
@@ -126,7 +149,7 @@ if __name__ == '__main__':
                         # Iterate through CSV.
                         for row in csv_parsed:
                             # Check if row contains data from specific tag.
-                            if row[5] == tag['sn'] and datetime.datetime.strptime(row[1], '%d.%m.%Y') >= args.startdate and datetime.datetime.strptime(row[1], '%d.%m.%Y') <= args.enddate:
+                            if row[5] == car['sn'] and datetime.datetime.strptime(row[1], '%d.%m.%Y') >= args.startdate and datetime.datetime.strptime(row[1], '%d.%m.%Y') <= args.enddate:
                                 # Add session to list.
                                 l_charge_sessions.append({
                                     'session_number': row[0],
@@ -136,27 +159,147 @@ if __name__ == '__main__':
                                     'energy': int(row[4])
                                 })
                         # Add sessions to global list.
-                        charge_sessions[tag['sn']] = l_charge_sessions
+                        charge_sessions[car['sn']] = l_charge_sessions
 
                         print('> Found ' + str(len(l_charge_sessions)) + ' accountable sessions.')
 
                     # Debug output.
                     print()
-                    print('Parsing finished. Display reports...')
+                    print('Parsing finished. Generating reports...')
                     print()
 
-                    # Iterate through tags and generate report.
-                    for sn in charge_sessions:
-                        print(lookup_plate(config['rfid_tags'], sn) + ' (' + sn + ') – ' + args.startdate.strftime('%d.%m.%Y') + ' - ' + args.enddate.strftime('%d.%m.%Y'))
+                    if args.output == 'cli':
+                        # Output on the CLI. Iterate through cars and generate report.
+                        for sn in charge_sessions:
+                            car = lookup_car(config['cars'], sn)
+                            print(car['plate'] + ' (' + sn + ') – ' + args.startdate.strftime('%d.%m.%Y') + ' - ' + args.enddate.strftime('%d.%m.%Y'))
 
-                        # Iterate though charging sessions.
-                        energy_sum = 0
-                        for session in charge_sessions[sn]:
-                            energy_sum += session['energy']
-                            print(session['start_date'] + "\t" + session['start_time'] + "\t" + str(round(session['energy'] / 1000, 2)) + 'kWh')
+                            # Iterate though charging sessions.
+                            energy_sum = 0
+                            for session in charge_sessions[sn]:
+                                energy_sum += session['energy']
+                                print(session['start_date'] + "\t" + session['start_time'] + "\t" + str(round(session['energy'] / 1000, 2)) + 'kWh')
 
-                        print('Σ ' + str(round(energy_sum / 1000, 2)) + 'kWh x ' + str(config['app']['cost']) + ' €/kWh = ' + str(round(energy_sum / 1000 * config['app']['cost'], 2)) + '€')
-                        print()
+                            print('Σ ' + str(round(energy_sum / 1000, 2)) + 'kWh x ' + str(config['power']['cost']) + ' €/kWh = ' + str(round(energy_sum / 1000 * config['power']['cost'], 2)) + '€')
+                            print()
+
+                    elif args.output == 'pdf':
+                        # Output as PDF.
+                        print('Generate PDF per car and save it to ' + os.path.abspath(os.path.dirname( __file__ )) + '/export' + ' folder.')
+
+                        # Iterate through cars and generate report.
+                        for sn in charge_sessions:
+                            car = lookup_car(config['cars'], sn)
+                            
+                            # Basic PDF setup.
+                            pdf_path = os.path.abspath(os.path.dirname( __file__ )) + '/export/' + args.startdate.strftime('%Y-%m-%d') + '-' + args.enddate.strftime('%Y-%m-%d') + '_' + car['plate'] + '.pdf'
+                            pdf_w, pdf_h = A4
+                            pdf = canvas.Canvas(
+                                pdf_path,
+                                pagesize=A4
+                            )
+                            pdf.setTitle('Verbrauchsabrechnung Strombezug')
+                            pdf.setFont('Helvetica', 10)
+
+                            # Add debitor to PDF.
+                            pdf_textblock = pdf.beginText(pdf_w * 0.1, pdf_h * 0.925)
+                            for line in car['debitor'].split("\n"):
+                                pdf_textblock.textLine(line)
+                            pdf.drawText(pdf_textblock)
+
+                            # Add headline and current date to PDF.
+                            pdf.drawCentredString(pdf_w / 2, pdf_h * 0.8, 'Verbrauchsabrechnung Strombezug')
+                            pdf.drawRightString(pdf_w * 0.9, pdf_h * 0.8, datetime.date.today().strftime('%d.%m.%Y'))
+
+                            # Add timerange data to PDF.
+                            pdf.drawString(pdf_w * 0.1, pdf_h * 0.77, 'Zeitraum:')
+                            pdf.drawString(pdf_w * 0.3, pdf_h * 0.77, args.startdate.strftime('%d.%m.%Y') + ' - ' + args.enddate.strftime('%d.%m.%Y'))                     
+
+                            # Add car data to PDF.
+                            pdf_textblock = pdf.beginText(pdf_w * 0.1, pdf_h * 0.75)
+                            pdf_textblock.textLine('Fahrzeug:')
+                            pdf_textblock.textLine('Kennzeichen:')
+                            pdf.drawText(pdf_textblock)
+                            pdf_textblock = pdf.beginText(pdf_w * 0.3, pdf_h * 0.75)
+                            pdf_textblock.textLine(car['type'])
+                            pdf_textblock.textLine(car['plate'])
+                            pdf.drawText(pdf_textblock)
+
+                            # Add outlet data to PDF.
+                            pdf_textblock = pdf.beginText(pdf_w * 0.1, pdf_h * 0.715)
+                            pdf_textblock.textLine('Verbrauchsstelle:')
+                            pdf_textblock.textLine('Nummer:')
+                            pdf.drawText(pdf_textblock)
+                            pdf_textblock = pdf.beginText(pdf_w * 0.3, pdf_h * 0.715)
+                            pdf_textblock.textLine(config['power']['model'])
+                            pdf_textblock.textLine(manuf_serial_n)
+                            pdf.drawText(pdf_textblock)                            
+
+                            # Add supplier data to PDF.
+                            pdf_textblock = pdf.beginText(pdf_w * 0.1, pdf_h * 0.68)
+                            pdf_textblock.textLine('Lieferrant:')
+                            pdf_textblock.textLine('Tarif:')
+                            pdf.drawText(pdf_textblock)
+                            pdf_textblock = pdf.beginText(pdf_w * 0.3, pdf_h * 0.68)
+                            pdf_textblock.textLine(config['power']['supplier'])
+                            pdf_textblock.textLine(config['power']['tarif'])
+                            pdf.drawText(pdf_textblock)
+
+                            # Add text.
+                            pdf_textblock = pdf.beginText(pdf_w * 0.1, pdf_h * 0.64)
+                            pdf_textblock.textLine('Der Zeitraum der Zählererfassung ist der o.g. Zeitraum jeweils von 00:00 bis 23:59 Uhr.')
+                            pdf_textblock.textLine('Ich bitte um Erstattung der Kosten im Rahmen des steuerfreien Auslagenersatzes.')
+                            pdf.drawText(pdf_textblock)
+
+                            # Add line.
+                            pdf.line(pdf_w * 0.1, pdf_h * 0.595, pdf_w * 0.9, pdf_h * 0.595)
+
+                            # Create table.
+                            pdf_table_date = pdf.beginText(pdf_w * 0.1, pdf_h * 0.55)
+                            pdf_table_date.textLine('Datum')
+                            pdf_table_date.textLine('')
+                            pdf_table_time = pdf.beginText(pdf_w * 0.2675, pdf_h * 0.55)
+                            pdf_table_time.textLine('Zeit')
+                            pdf_table_time.textLine('')
+                            pdf_table_duration = pdf.beginText(pdf_w * 0.4, pdf_h * 0.55)
+                            pdf_table_duration.textLine('Dauer')
+                            pdf_table_duration.textLine('')
+                            pdf_table_usage = pdf.beginText(pdf_w * 0.55, pdf_h * 0.55)
+                            pdf_table_usage.textLine('Verbrauch')
+                            pdf_table_usage.textLine('')
+                            pdf_table_costs = pdf.beginText(pdf_w * 0.75, pdf_h * 0.55)
+                            pdf_table_costs.textLine('Kosten')
+                            pdf_table_costs.textLine('')
+
+                            # Iterate though charging sessions.
+                            energy_sum = 0
+                            for session in charge_sessions[sn]:
+                                energy_sum += session['energy']
+                                pdf_table_date.textLine(session['start_date'])
+                                pdf_table_time.textLine(session['start_time'])
+                                pdf_table_duration.textLine(session['duration'])
+                                pdf_table_usage.textLine(str(round(session['energy'] / 1000, 2)) + ' kWh')
+                                pdf_table_costs.textLine(str(round(session['energy'] / 1000 * config['power']['cost'], 2)) + ' €')
+
+                            
+                            # Add sum to the end.
+                            pdf_table_usage.textLine('')
+                            pdf_table_usage.textLine(str(round(energy_sum / 1000, 2)) + ' kWh')
+                            pdf_table_costs.textLine('')
+                            pdf_table_costs.textLine(str(round(energy_sum / 1000 * config['power']['cost'], 2)) + ' €')
+
+                            # Render table.
+                            pdf.drawText(pdf_table_date)
+                            pdf.drawText(pdf_table_time)
+                            pdf.drawText(pdf_table_duration)
+                            pdf.drawText(pdf_table_usage)
+                            pdf.drawText(pdf_table_costs)
+
+
+
+                            # Render PDF
+                            pdf.showPage()
+                            pdf.save()
                     
                 else:
                     quit('Charge report could not be downloaded.')
